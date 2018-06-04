dcaefb3aecc37703a608f02d03b1ccded058eb7c
GBTC440003UR <fadhld92@gmail.com>
2018-06-04 16:15:33 -0400

    refactored response method

--- src/com/company/Eliza.java
+++ src/com/company/Eliza.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 import java.util.Random;
-
 public class Eliza {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         String prompt = "";
-        System.out.print("Good day. What is your problem?");
-        {
-            while (!prompt.equals("Q") || !prompt.equals("q")) {
-                System.out.println("Enter your response here or Q to quit:");
-                prompt = in.nextLine();
-                if (prompt.equals("Q") || prompt.equals("q")) break;
-                System.out.println(respond(prompt));
-            }
+        System.out.print("Good day. What is your problem? " );
+        while (!prompt.equals("Q") || !prompt.equals("q")) {
+            System.out.print("Enter your response here or Q to quit: ");
+            prompt = in.nextLine();
+            if (prompt.equals("Q") || prompt.equals("q"))  break;
+            System.out.println(respond(prompt));
         }
+
     }
 
     private static String respond(String prompt) {
@@ -24,38 +21,35 @@ public class Eliza {
         int responseType = rnd.nextInt(3);
         if (responseType == 1) {
             response = replacePronouns(response);
-            response = addQualifiers(response, rnd);
-        } else {
+            response = addQualifier(response, rnd);
+        }
+        else {
             response = hedge(rnd);
-
         }
         return response;
     }
 
     private static String hedge(Random rnd) {
         String response;
-        String[] hedges = {"Please tell me more", "Many of my patients tell me the same thing", "It is getting late, maybe we had better quit"};
-        response = hedges[rnd.nextInt(hedges.length)];
+        String [] hedges = {"Please tell me more", "Many of my patients tell me the same thing", "It is getting late, maybe we had better quit"};
+        response = hedges [rnd.nextInt(hedges.length)];
         return response;
     }
 
-    private static String addQualifiers(String response, Random rnd) {
-        String[] qualifiers = {"Why do you say that", "You seem to think that", "So, you are concerned that"};
-        response = qualifiers[rnd.nextInt(qualifiers.length)] + " " + response + "?";
+    private static String addQualifier(String response, Random rnd) {
+        String [] qualifiers = {"Why do you say that", "You seem to think that", "So, you are concerned that"};
+        response =  qualifiers [rnd.nextInt(qualifiers.length)]+ " " + response + "?";
         return response;
     }
 
     private static String replacePronouns(String response) {
-        String[] responses = {"me", "my", "I", "am"};
-        String[] replacements = {"you", "your", "you", "are"};
+        String [] responses = {"me", "my", "I", "am"};
+        String [] replacements = {"you", "your", "you", "are"};
         for (int i = 0; i < responses.length; i++) {
-
-            response = response.replace(responses[i], replacements[i]);
-
+            if (response.contains (" " + responses[i] )|| response.contains(responses[i] + " ") || response.equals(responses[i])) {
+                response = response.replaceAll(responses[i], replacements[i]);
+            }
         }
-            return response;
-        }
-
-
-
+        return response;
+    }
 }
