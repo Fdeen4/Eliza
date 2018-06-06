9c4020982974cf2e2737d81230a102f40e83328d
GBTC441015UR <fadhld92@gmail.com>
2018-06-06 13:53:24 -0400

    pig latin and red

--- src/com/company/Eliza.java
+++ src/com/company/Eliza.java
@@ -1,21 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Random;
-import java.util.HashMap;
+import java.util.*;
+
 public class Eliza {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String prompt = "";
-        System.out.print("Good day. What is your problem? " );
+        System.out.print("Good day. \nWhat is your problem?\n" );
+        int counter = 0;
         while (!prompt.equals("Q") || !prompt.equals("q")) {
             System.out.print("Enter your response here or Q to quit: ");
             prompt = in.nextLine();
-            if (prompt.equals("Q") || prompt.equals("q"))  break;
-            System.out.println(respond(prompt));
-        }
+            //if (prompt.equalsIgnoreCase("game")) {
 
+             if (prompt.equals("Q") || prompt.equals("q"))  break;
+            String finalResponse = respond(prompt);
+            System.out.println(finalResponse);
+            counter++;
+            if (counter==8) {
+                System.out.println("It is getting late, maybe we had better quit.");
+                break;}
+        }
+        System.exit(0);
     }
 
     private static String respond(String prompt) {
@@ -29,19 +34,31 @@ public class Eliza {
         else if(responseType == 2){
             response = hedge(rnd);
         }
-        if (prompt.contains ("caps")) return response.replaceAll ("caps", "").toUpperCase();
-        else if (prompt.contains ("lower case")) return response.replaceAll ("lower case", "").toLowerCase();
+        if (prompt.equals("pig latin")){
+            PigLatin pig = new PigLatin();
+            return pig.create(response);
+        }
+        if (prompt.equalsIgnoreCase ("caps")) return response.replaceAll ("caps", "").toUpperCase();
+        else if (prompt.equalsIgnoreCase ("lower case")) return response.replaceAll ("lower case", "").toLowerCase();
+        else if (prompt.equalsIgnoreCase ("red")) {
+            RedText rd = new RedText();
+            return rd.makeRed(response);
+        }
         else return response;
     }
 
     private static String hedge(Random rnd) {
-        String response;
         String [] hedgesA = {"Please tell me more",
                 "Many of my patients tell me the same thing",
                 "It is getting late, maybe we had better quit"};
         ArrayList<String> hedges = new ArrayList<String>(Arrays.asList(hedgesA));
-        response = hedges.get (rnd.nextInt(hedges.size()));
-        return response;
+        HashSet<String> hedgesHash = new HashSet<>(hedges);
+        Iterator it = hedgesHash.iterator();
+        int responseId = rnd.nextInt(hedgesHash.size());
+        for (int i = 0; i < responseId; i++) {
+            it.next();
+        }
+        return it.next().toString();
     }
 
     private static String addQualifier(String response, Random rnd) {
@@ -56,7 +73,7 @@ public class Eliza {
         ArrayList<String> responses = new ArrayList<String>(Arrays.asList(responsesA));
         HashMap<String, String> replacementResponses = new HashMap();
         String [] replacementsA = {"you", "your", "you", "are"};
-        ArrayList<String> replacements = new ArrayList<String>(Arrays.asList( replacementsA));
+        //ArrayList<String> replacements = new ArrayList<String>(Arrays.asList( replacementsA));
         for (int i = 0; i < responsesA.length; i++){
             replacementResponses.put(responsesA[i],replacementsA[i]);
         }
